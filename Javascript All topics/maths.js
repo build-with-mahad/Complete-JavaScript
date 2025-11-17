c002a1e67de86d31b6caddb45e64186116460822
build-with-mahad <mahadkhan122005@gmail.com>
2025-11-18 03:45:56 +0500

    add math operations and formatting examples

---
+++ Javascript All topics/maths.js
@@ -0,0 +1,14 @@
+const number = 100
+console.log(number.toFixed(2))
+let num = 25.45
+console.log(num.toPrecision(2))
+console.log(Math.round(25.30))
+console.log(Math.ceil(45.34))
+console.log(Math.ceil(45.34))
+console.log(Math.floor(4.9))
+console.log(Math.abs(-10))
+console.log(Math.max(10,20,39,556,342))
+console.log(Math.min(-10,20,39,556,342))
+console.log(Math.floor(Math.random()*10 + 1))
+const number1 = 1453672
+console.log(number1.toLocaleString('en-PK'))
