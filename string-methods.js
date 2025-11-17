05957a4f0fe98e51a383d228eff38593cc26b6e7
Mahad Khan <mahad@gmail.com>
2025-11-18 02:52:59 +0500

    first commit

---
+++ string-methods.js
@@ -0,0 +1,17 @@
+const string = " Muhammad Mahad Khan "
+console.log(string.at(0))
+console.log(string.charAt(4))
+console.log(string.replace("M","D"))
+console.log(string.replaceAll("M","V"))
+console.log(string.includes("d"))
+console.log(string.startsWith("M"))
+console.log(string.endsWith("n"))
+console.log(string.slice(0,8))
+console.log(string.trimStart())
+console.log(string.trimEnd())
+console.log(string.indexOf("d"))
+console.log(string.toUpperCase())
+console.log(string.toLowerCase())
+const string1 = " Muhammad-Mahad-Khan "
+console.log(string1.split("-"))
+
